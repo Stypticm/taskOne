f13adda1e4a9c1bef3f4a968de6241d79ebb9e44
Mikhail Volodin <33923379+Stypticm@users.noreply.github.com>
2022-05-10 18:37:12 +0300

    Add token

--- Router/authRouter.js
+++ Router/authRouter.js
@@ -1,12 +1,17 @@
 import Router from 'express'
 import AuthController from '../Controller/AuthController.js';
-import {check} from "express-validator";
+import {
+    check
+} from "express-validator";
 
 const authRouter = new Router();
 
 authRouter.post('/registration', [
     check('login', "Имя пользователя не может быть пустым").notEmpty(),
-    check('password', "Пароль не может быть меньше 4 и больше 10 символов").isLength({min: 4, max: 10})
+    check('password', "Пароль не может быть меньше 4 и больше 10 символов").isLength({
+        min: 4,
+        max: 10
+    })
 ], AuthController.registration)
 authRouter.post('/login', AuthController.login)
 
